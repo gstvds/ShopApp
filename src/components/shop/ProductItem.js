b614ab8cf25f20aed87a116fce3c78ff91cdaf9c
Gustavo da Silva <gstvds@icloud.com>
2019-11-05 08:29:55 -0300

    Added shop components like Cart, Orders and Products

---
+++ src/components/shop/ProductItem.js
@@ -0,0 +1,99 @@
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform
+} from "react-native";
+
+import Colors from "../../constants/Colors";
+
+const ProductItem = props => {
+  const TouchableCmp = TouchableOpacity;
+  if (Platform.OS === "android" && Platform.Version >= 21) {
+    TouchableCmp = TouchableNativeFeedback;
+  }
+  return (
+    <View style={styles.product}>
+      <View style={styles.touchable}>
+        <TouchableCmp onPress={props.onViewDetail} useForeground>
+          <View style={styles.imageContainer}>
+            <Image style={styles.image} source={{ uri: props.image }} />
+          </View>
+          <View style={styles.detail}>
+            <Text style={styles.title}>{props.title}</Text>
+            <Text style={styles.price}>${props.price.toFixed(2)}</Text>
+          </View>
+          <View style={styles.actions}>
+            <Button
+              color={Colors.primary}
+              title="VIEW DETAIL"
+              onPress={props.onViewDetail}
+            />
+            <Button
+              color={Colors.primary}
+              title="TO CART"
+              onPress={props.onAddToCart}
+            />
+          </View>
+        </TouchableCmp>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  product: {
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: "white",
+    height: 300,
+    margin: 20
+  },
+  touchable: {
+    overflow: "hidden",
+    borderRadius: 10
+  },
+  imageContainer: {
+    width: "100%",
+    height: "60%",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    overflow: "hidden"
+  },
+  image: {
+    width: "100%",
+    height: "100%"
+  },
+  detail: {
+    alignItems: "center",
+    height: "15%",
+    padding: 10
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 18,
+    marginVertical: 4
+  },
+  price: {
+    fontFamily: "open-sans",
+    fontSize: 14,
+    color: "#888"
+  },
+  actions: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    height: "25%"
+  }
+});
+
+export default ProductItem;
