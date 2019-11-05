17c1d4cd836ffd696eccbc054831d8a3d41eeb87
Gustavo da Silva <gstvds@icloud.com>
2019-11-05 08:33:40 -0300

    Added custom fonts and cart/orders reducers to combineReducers

--- App.js
+++ App.js
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { StatusBar } from "react-native";
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
 
 import productsReducer from "./src/store/reducers/products";
+import cartReducer from "./src/store/reducers/cart";
+import ordersReducer from "./src/store/reducers/orders";
 import ShopNavigator from "./src/navigation/ShopNavigator";
 
 const rootReducer = combineReducers({
-  products: productsReducer
+  products: productsReducer,
+  cart: cartReducer,
+  orders: ordersReducer
 });
 
 const store = createStore(rootReducer);
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./src/assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./src/assets/fonts/OpenSans-Bold.ttf")
+  });
+};
+
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
+      />
+    );
+  }
+
   return (
     <Provider store={store}>
+      <StatusBar barStyle="dark-content" />
       <ShopNavigator />
     </Provider>
   );
--- README.md
+++ README.md
@@ -13,3 +13,16 @@ expo install react-native-gesture-handler react-native-reanimated
 First we need to connect react-redux with our app, using createStore, combineReducers and Provider. Then we create our reducer using our dummy-data to provide our initialState
 
 Then, to start with navigation, we create our navigator using createStackNavigator imported from 'react-navigation-stack'
+
+In order to create our products screen, we need a product component. We could create this component inside our screen file, but as our product isn't small, we create a separated file to store this component (= ProductItem.js).
+To fix our price with two decimal numbers, we can use toFixed(2).
+
+To use custom fonts, we first need to add expo-fonts by installing it
+
+```bash
+expo install expo-fonts
+```
+
+Then create a constant to store our fonts in a javascript object inside Font.loadAsync. Then, with useState and AppLoading we set our app to start after the font is loaded.
+
+To manage our cart products we will also use redux. So we create a new reducer/action called cart
