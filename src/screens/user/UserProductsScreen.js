f6d34dba54c6ffc3a24e7eb4579568f20e946c73
Gustavo da Silva <gstvds@icloud.com>
2019-11-05 09:02:52 -0300

    Added Admin screen

--- src/screens/user/UserProductsScreen.js
+++ src/screens/user/UserProductsScreen.js
@@ -1,18 +1,49 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { FlatList, Button, Platform } from "react-native";
+import { useSelector } from "react-redux";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
+import ProductItem from "../../components/shop/ProductItem";
+import HeaderButton from "../../components/UI/HeaderButton";
+import Colors from "../../constants/Colors";
 
 const UserProductsScreen = props => {
+  const userProducts = useSelector(state => state.products.userProducts);
+
   return (
-    <View>
-      <Text>User Products Screen</Text>
-    </View>
+    <FlatList
+      data={userProducts}
+      keyExtractor={item => item.id}
+      renderItem={itemData => (
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onSelect={() => {}}
+        >
+          <Button color={Colors.primary} title="EDIT" onPress={() => {}} />
+          <Button color={Colors.primary} title="DELETE" onPress={() => {}} />
+        </ProductItem>
+      )}
+    />
   );
 };
 
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1
-  }
-});
+UserProductsScreen.navigationOptions = navData => {
+  return {
+    headerTitle: "Your Products",
+    headerLeft: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    )
+  };
+};
 
 export default UserProductsScreen;
