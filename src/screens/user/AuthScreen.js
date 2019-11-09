21cfdc5b3e039cf16a4d06bce26d22431838532a
Gustavo da Silva <gstvds@icloud.com>
2019-11-08 23:30:34 -0300

    Added login/signup authentication with firebase auth api

--- src/screens/user/AuthScreen.js
+++ src/screens/user/AuthScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useReducer, useCallback } from "react";
 import {
   ScrollView,
   View,
@@ -7,12 +7,83 @@ import {
   Button
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { useDispatch } from "react-redux";
 
 import Input from "../../components/UI/Input";
 import Card from "../../components/UI/Card";
 import Colors from "../../constants/Colors";
+import * as authActions from "../../store/actions/auth";
+
+const FORM_INPUT_UPDATE = "FORM_INPUT_UPDATE";
+
+const formReducer = (state, action) => {
+  if (action.type === FORM_INPUT_UPDATE) {
+    const updatedValues = {
+      ...state.inputValues,
+      [action.input]: action.value
+    };
+    const updatedValidities = {
+      ...state.inputValidities,
+      [action.input]: action.isValid
+    };
+    let updatedFormIsValid = true;
+    for (const key in updatedValidities) {
+      updatedFormIsValid = updatedFormIsValid && updatedValidities[key];
+    }
+    return {
+      formIsValid: updatedFormIsValid,
+      inputValidities: updatedValidities,
+      inputValues: updatedValues
+    };
+  }
+  return state;
+};
 
 const AuthScreen = props => {
+  const [isSignup, setIsSignup] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const [formState, dispatchFormState] = useReducer(formReducer, {
+    inputValues: {
+      email: "",
+      password: ""
+    },
+    inputValidities: {
+      email: false,
+      password: false
+    },
+    formIsValid: false
+  });
+
+  const authHandler = () => {
+    let action;
+    if (isSignup) {
+      action = authActions.signup(
+        formState.inputValues.email,
+        formState.inputValues.password
+      );
+    } else {
+      action = authActions.login(
+        formState.inputValues.email,
+        formState.inputValues.password
+      );
+    }
+    dispatch(action);
+  };
+
+  const inputChangeHandler = useCallback(
+    (inputIdentifier, inputValue, inputValidity) => {
+      dispatchFormState({
+        type: FORM_INPUT_UPDATE,
+        value: inputValue,
+        isValid: inputValidity,
+        input: inputIdentifier
+      });
+    },
+    [dispatchFormState]
+  );
+
   return (
     <KeyboardAvoidingView
       behavior="padding"
@@ -29,8 +100,8 @@ const AuthScreen = props => {
               required
               email
               autoCapitalize="none"
-              errorMessage="Please enter a valid email address."
-              onValueChange={() => {}}
+              errorText="Please enter a valid email address."
+              onInputChange={inputChangeHandler}
               initialValue=""
             />
             <Input
@@ -41,23 +112,25 @@ const AuthScreen = props => {
               required
               minLength={5}
               autoCapitalize="none"
-              errorMessage="Please enter a valid password."
-              onValueChange={() => {}}
+              errorText="Please enter a valid password."
+              onInputChange={inputChangeHandler}
               initialValue=""
             />
             <View style={styles.buttons}>
               <View style={styles.buttonContainer}>
                 <Button
-                  title="Login"
+                  title={isSignup ? "Sign Up" : "Login"}
                   color={Colors.primary}
-                  onPress={() => {}}
+                  onPress={authHandler}
                 />
               </View>
               <View style={styles.buttonContainer}>
                 <Button
-                  title="Sign Up"
+                  title={`Switch to ${isSignup ? "Login" : "Sign Up"}`}
                   color={Colors.accent}
-                  onPress={() => {}}
+                  onPress={() => {
+                    setIsSignup(prevState => !prevState);
+                  }}
                 />
               </View>
             </View>
